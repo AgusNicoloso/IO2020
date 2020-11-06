5410777a542d1cf7952c0cd3ffaaa7317250d3c9
AgusNicoloso <agustinnicoloso@uxorit.com>
2020-11-06 20:14:52 -0300

    Cosas que faltaban

--- logic.js
+++ logic.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
   $("#add").click(function () {
-    var lastField = $("#formulario div:last");
+    var lastField = $("#botonesdinamicos div:last");
+    console.log(lastField);
     var intId =
       (lastField && lastField.length && lastField.data("idx") + 1) || 1;
-    var fieldWrapper = $('<div id="field' + intId + '"/>');
+    var fieldWrapper = $(`
+  <div  style='margin-top: 10px;' class='input-group mb-3' id='field${intId}' />`);
     fieldWrapper.data("idx", intId);
-    var fName = $("<input type='text' placeholder='Punto nro " + intId + "'>");
+    var fName = $(
+      "<input type='text' class='form-control' id='aux" +
+        intId +
+        "'" +
+        "placeholder='Punto intermedio numero " +
+        intId +
+        "'>"
+    );
     var removeButton = $(
-      '<input type="button" class="btn btn-danger" value="Eliminar" />'
+      `<a class="input-group-append"> <button class="btn btn-danger" type="button">Eliminar</button> </a>`
     );
     removeButton.click(function () {
       $(this).parent().remove();
     });
     fieldWrapper.append(fName);
     fieldWrapper.append(removeButton);
-    $("#formulario").append(fieldWrapper);
+    $("#botonesdinamicos").append(fieldWrapper);
   });
 });
